efe0c0258b821e89093280144baa2398c332a497
一 辛 <Archi-BC@outlook.com>
2024-12-11 14:58:51 +0800

    项目初始化

---
+++ ABC/ABC_Rhino/ABC_RhinoCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Rhino;
+using Rhino.Commands;
+using Rhino.Geometry;
+using Rhino.Input;
+using Rhino.Input.Custom;
+
+namespace ABC_Rhino
+{
+    public class ABC_RhinoCommand : Command
+    {
+        public ABC_RhinoCommand()
+        {
+            // Rhino only creates one instance of each command class defined in a
+            // plug-in, so it is safe to store a refence in a static property.
+            Instance = this;
+        }
+
+        ///<summary>The only instance of this command.</summary>
+        public static ABC_RhinoCommand Instance { get; private set; }
+
+        ///<returns>The command name as it appears on the Rhino command line.</returns>
+        public override string EnglishName => "ABC_RhinoCommand";
+
+        protected override Result RunCommand(RhinoDoc doc, RunMode mode)
+        {
+            RhinoApp.WriteLine("The {0} command is aaaaaa.", EnglishName);
+            return Result.Success;
+        }
+    }
+}
